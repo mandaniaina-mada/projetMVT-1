d21f9198c3bf4ffa7e07ace8a1da5ee600465bcc
Mandaniaina <mandaniaina.randriambinintsoa@esti.mg>
2020-03-09 14:46:19 +0300

    first commit

---
+++ src/main/java/mg/esti/projetMVT/ProjetMvt1Application.java
@@ -0,0 +1,13 @@
+package mg.esti.projetMVT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMvt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMvt1Application.class, args);
+	}
+
+}
---
+++ src/test/java/mg/esti/projetMVT/ProjetMvt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.esti.projetMVT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMvt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
